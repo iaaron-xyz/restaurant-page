79d83f3fd4d41771c122cb28ee389bb4b3c76c5d
Aaron <iaaron.xyz@outlook.com>
2023-01-21 21:54:51 -0600

    generate HTML using Webpack and JavaScript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import {menuTest} from './menu.js';
 import './styles.css';
 
+// get the body element
+const body = document.getElementsByTagName('body')[0];
+
+// Generate the main continer
+const container = document.createElement('div');
+// Add it an id
+container.setAttribute('id', 'container');
+// Render the container div
+body.appendChild(container);
+
+// Insert text inside the container
+container.textContent = 'This is a generated text';
+
 console.log('This restaurnat page is working');
 console.log(menuTest());
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+    }),
+  ],
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
@@ -10,7 +16,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    //clean: true,
+    clean: true,
   },
   module: {
     rules: [
