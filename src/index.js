0c7871fb32c074a867c514804053172f0321dfa4
Aaron <iaaron.xyz@outlook.com>
2023-01-22 01:06:53 -0600

    add tailwind configuration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import { homeTest } from './home';
 import { menuTest } from './menu';
 import { contactTest } from './contact';
-import './styles.css';
+import './mean.css';
 
 // get the body element
+const html = document.getElementsByTagName('html')[0];
 const body = document.getElementsByTagName('body')[0];
+// Set attributes to body and html
+body.setAttribute('class', 'h-full w-full');
+html.setAttribute('class', 'h-full w-full box-border bg-black text-white border-solid border-2 border-sky-500');
 
 // Generate the main continer
 const container = document.createElement('div');
 // Add it an id
 container.setAttribute('id', 'container');
+container.setAttribute('class', 'text-3xl font-bold underline');
 // Render the container div
 body.appendChild(container);
 
