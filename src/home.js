806f5f6b171601eb2fb9c6268a85c0903390d8ad
Aaron <iaaron.xyz@outlook.com>
2023-01-22 18:40:22 -0600

    add home basic layout

--- src/home.js
+++ src/home.js
@@ -1,5 +1,18 @@
-function homeTest() {
-  return 'Hello from the home section!';
+function createHomeContainer() {
+  // Get DOM elements
+  const container = document.getElementById('container');
+
+  // manipulate element
+  const homeContainer = document.createElement('div');
+  homeContainer.setAttribute('class', 'flex grow items-center justify-center text-8xl');
+  homeContainer.setAttribute('id', 'home-container');
+  // Add content
+  homeContainer.innerHTML = 'Home Container';
+
+  // Add element to the DOM
+  container.appendChild(homeContainer);
+
+  console.log(container);
 }
 
-export { homeTest };
+export { createHomeContainer };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createNav, createNavItems } from './navbar';
-// import { homeTest } from './home';
+import { createHomeContainer } from './home';
 // import { menuTest } from './menu';
 // import { contactTest } from './contact';
 import './mean.css';
@@ -15,11 +15,12 @@ function initializeHtml() {
   // Set attributes to body and html
   body.setAttribute('class', 'h-full w-full');
   html.setAttribute('class', 'h-full w-full box-border bg-black text-white');
+  // html.setAttribute('class', 'h-full w-full');
   // Generate the main continer
   const container = document.createElement('div');
   // Add id and classes
   container.setAttribute('id', 'container');
-  container.setAttribute('class', 'text-5xl');
+  container.setAttribute('class', 'text-4xl h-full w-full flex flex-col');
 
   // Render the container div
   body.appendChild(container);
@@ -34,6 +35,11 @@ function initializeHtml() {
 const baseElements = initializeHtml();
 createNav(baseElements.container);
 createNavItems();
+
+/**
+ * Add Home content
+ */
+createHomeContainer();
 
 // Insert text inside the container
 // container.textContent = 'This is a generated text';
