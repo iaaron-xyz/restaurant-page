86184058a9e5041d66378b35b553689d623439af
Aaron <iaaron.xyz@outlook.com>
2023-01-23 15:48:02 -0600

    Basic layout and style to menu section

--- src/menu.js
+++ src/menu.js
@@ -6,12 +6,23 @@ function createMenu() {
   const menuContainer = document.createElement('div');
   // Set attributes to the menu container
   menuContainer.setAttribute('id', 'menu-container');
-  menuContainer.setAttribute('class', 'bg-slate-200 flex flex-col items-center justify-center mt-28 text-6xl hidden');
+  menuContainer.setAttribute('class', 'bg-slate-200 flex flex-col items-center justify-center mt-24 pb-24 text-6xl hidden');
   // create menu elements
-  menuContainer.innerHTML = '<h1>Menu Section</h1>'
-    + '<div class="text-2xl">'
-    + '<p>Plate 1</p>'
-    + '</div>';
+  menuContainer.innerHTML = '<h1 class="mt-12">Menu Section</h1>';
+  menuContainer.innerHTML += '<div class="mt-12 text-base content-font w-1/3">'
+                              + '<h2 class="text-3xl">Plate 1</h2>'
+                              + '<div class="grid grid-cols-3 gap-4">'
+                                + '<p class="col-span-2">Description lorem ipsun athem gotta found</p>'
+                                + '<p class="number-font place-self-center">$100.00</p>'
+                              + '</div>'
+                            + '</div>';
+  menuContainer.innerHTML += '<div class="mt-12 text-base content-font w-1/3">'
+                              + '<h2 class="text-3xl">Plate 1</h2>'
+                                + '<div class="grid grid-cols-3 gap-4">'
+                                  + '<p class="col-span-2">Description lorem ipsun athem gotta found</p>'
+                                  + '<p class="number-font place-self-center">$100.00</p>'
+                                + '</div>'
+                            + '</div>';
   // Append the menu element to the DOM
   container.appendChild(menuContainer);
 }
